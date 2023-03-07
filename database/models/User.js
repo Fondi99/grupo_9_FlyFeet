c140ce310d1a39e16eef20640a88182b127bbce0
Lautaro Serrano <lautaroserrano@gmail.com>
2023-03-06 22:32:57 -0300

    Remove unnecessary features

--- database/models/User.js
+++ database/models/User.js
@@ -1,53 +1,54 @@
-const model = function (sequelize, DataTypes) {
-    let modelName, attributes, options;
-    modelName = "User"
-    attributes = {
-        id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        name: {
-            type: DataTypes.STRING
-        },
-        password: {
-            type: DataTypes.STRING
-        },
-        email: {
-            type: DataTypes.STRING
-        },
-        address: {
-            type: DataTypes.STRING
-        },
-        zip: {
-            type: DataTypes.DECIMAL
-        },
-        phone_number: {
-            type: DataTypes.DECIMAL
-        },
-        city: {
-            type: DataTypes.STRING
-        },
-        role: {
-            type: DataTypes.STRING
-        }
+const model = function (sequelize, dataTypes) {
+  let modelName, attributes, options;
+  modelName = "User";
+  attributes = {
+    id: {
+      type: dataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    first_name: {
+      type: dataTypes.STRING,
+      allowNull: false,
+    },
+    last_name: {
+      type: dataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: dataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    password: {
+      type: dataTypes.STRING,
+      allowNull: false,
+    },
+    role: {
+      type: dataTypes.ENUM,
+      values: ["customer", "admin"],
+      allowNull: false,
+    },
+    created_at: {
+      type: dataTypes.DATE,
+      defaultValue: dataTypes.NOW,
+    },
+    modified_at: {
+      type: dataTypes.DATE,
+      defaultValue: dataTypes.NOW,
     }
-    options = {
-        tableName: "users",
-        createdAt: 'created_at',
-        updatedAt: 'updated_at',
-        charset: "utf8mb4",
-        collate: "utf8mb4_unicode_ci"
-    };
+  };
+  options = {
+    tableName: "users",
+    createdAt: "created_at",
+    updatedAt: "updated_at",
+    charset: "utf8mb4",
+    collate: "utf8mb4_unicode_ci",
+  };
 
-    const User = sequelize.define(modelName, attributes, options);
+  const User = sequelize.define(modelName, attributes, options);
 
-    User.associate = function (models) {
-        User.hasMany(models.Payment, {
-            as: "pedidos",
-            foreignKey: "user_id"
-        });
-    }
-    return User;
-}
+  return User;
+};
+
 export default model;
