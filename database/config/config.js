81d3014d94126fd46f113bf016b02ced3bc73397
RodrigoTalledo <rotel.wolf@gmail.com>
2023-03-18 12:07:05 -0300

    arreglos

--- database/config/config.js
+++ database/config/config.js
@@ -1,10 +1,6 @@
 export default {
   username: "root",
-<<<<<<< HEAD
   password: "",
-=======
-  password: "toor",
->>>>>>> dc86f12e83efdeb7b30af9e860723c0999eaca8c
   database: "flyfeet_db",
   host: "127.0.0.1",
   dialect: "mysql",
